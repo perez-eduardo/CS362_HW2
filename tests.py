15485800a16296fcb857770e513ce86a027255e2
perez-eduardo <eduardo.perez.student@gmail.com>
2025-10-24 16:56:42 -0700

    Refactor test cases for contrived_func
    Updated test suite to achieve full branch and condition coverage by renaming and modifying tests.

--- tests.py
+++ tests.py
@@ -3,29 +3,38 @@ from contrived_func import contrived_func
 
 
 class TestContrivedFunc(unittest.TestCase):
-    """Test suite with 6 tests - removed val=2 and val=3."""
+    """Test suite for contrived_func achieving full branch and condition
+    coverage."""
 
     def test_1(self):
         """Test with a=T, b=T, c=F, d=T."""
         contrived_func(1)
 
     def test_2(self):
+        """Test with a=T, b=T, c=F, d=F."""
+        contrived_func(2)
+
+    def test_3(self):
         """Test with a=T, b=F, c=F, d=T."""
         contrived_func(0)
 
-    def test_3(self):
+    def test_4(self):
+        """Test with a=T, b=F, c=F, d=F."""
+        contrived_func(3)
+
+    def test_5(self):
         """Test with a=F, b=T, c=T, d=F."""
         contrived_func(40)
 
-    def test_4(self):
+    def test_6(self):
         """Test with a=F, b=T, c=F, d=F."""
         contrived_func(20)
 
-    def test_5(self):
+    def test_7(self):
         """Test with a=F, b=F, c=T, d=F."""
         contrived_func(42)
 
-    def test_6(self):
+    def test_8(self):
         """Test with a=F, b=F, c=F, d=F."""
         contrived_func(36)
 
